3ac56387cb4147f019468e459d249f1343d1bf2e
Jimmy Payne <jamespayne@users.noreply.github.com>
2022-12-08 23:49:55 +1100

    Update Read.ps1

--- Spreadsheets and CSV files/Excel/Read.ps1
+++ Spreadsheets and CSV files/Excel/Read.ps1
@@ -1,5 +1,5 @@
 #Author: Jimmy Payne
-#Basic script to read the contents of a .xlsx file
+#Basic script to read the contents of a .xlsx file and see what data is there.
 #Reference: https://devblogs.microsoft.com/scripting/grabbing-excel-xlsx-values-with-powershell/
 #Required Modules: PSExcel
 
@@ -15,9 +15,12 @@ foreach ($record in (Import-XLSX -Path $WorkingFile -RowStart 1)){
     Write-Host $record
 }
 
+#Note: The -RowSart 1 parameter allows us to skip the header row which contains:
+
+Product	Category	Price	Stock	Comments
+
 #If everything is in order, you should see the following results:
 
 #@{Product=Cat Food Deluxe; Category=Cat; Price=49.99; Stock=1; Comments=Best cat food ever}
 #@{Product=Dog Food Deluxe; Category=Dog; Price=38.48; Stock=56; Comments=Best dog food ever}
-#@{Product=Blue-Tounge Food Deluxe; Category=Blue-Tounge Lizared; Price=45.99; Stock=23; Comments=Best Blue-Tounge food ever}
-
+#@{Product=Blue-Tounge Food Deluxe; Category=Blue-Tounge Lizard; Price=45.99; Stock=23; Comments=Best Blue-Tounge food ever}
